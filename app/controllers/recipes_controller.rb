ef023fb157527d09f92a7373f599c513532f574b
Rob Jewell <jewell.robertp@gmail.com>
2020-04-05 17:20:37 -0400

    set up recipe CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,18 @@
 class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(
+      title: params[:recipe][:title],
+      description: params[:recipe][:description]
+    )
+
+    redirect_to @recipe
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
