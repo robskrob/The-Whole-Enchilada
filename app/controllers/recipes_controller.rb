14f198c2cb4df41c850ec7a8aad90513b46360f6
Rob Jewell <jewell.robertp@gmail.com>
2020-04-09 08:29:22 -0400

    recipe create form with file upload
    and file attachment save abilities

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,11 +4,47 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.create(
+    @recipe = Recipe.new(
       title: params[:recipe][:title],
       description: params[:recipe][:description]
     )
 
+    @recipe.save
+
+    # blog post on how to avoid multiple inserts
+    # and how to add multiple file attachments
+    # per image insert
+    # but in this instance I may have to tolerate
+    # multiple inserts.
+
+#    params[:recipe][:images][:file].map do |image|
+#      image = @recipe.images.new
+#
+#      image.save
+#
+#      image.file.attach(
+#        io: File.open(image.path),
+#        filename: image.original_filename,
+#        content_type: image.content_type
+#      )
+#    end
+
+#    images = params[:recipe][:images].map do |image|
+#    end
+
+
+    if params[:recipe][:images].present? && params[:recipe][:images][:file].present?
+      image = @recipe.images.create
+      file = params[:recipe][:images][:file]
+
+      image.file.attach(
+        io: File.open(file.path),
+        filename: file.original_filename,
+        content_type: file.content_type
+      )
+    end
+
+#    @stub_recipe = Recipe.find(4)
     redirect_to @recipe
   end
 
