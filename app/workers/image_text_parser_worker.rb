1520fc2734c5e2169a6c5a3a03a2cdfc1c913803
Rob Jewell <jewell.robertp@gmail.com>
2020-04-12 16:32:07 -0400

    WIP: parse text from image in background

--- app/workers/image_text_parser_worker.rb
+++ app/workers/image_text_parser_worker.rb
@@ -1,16 +1,24 @@
+require 'open-uri'
+
 class ImageTextParserWorker
   include Sidekiq::Worker
 
   def perform(image_id)
-#    image = Image.find(image_id)
-#
-#    image = RTesseract.new(image.path)
-#
-#    @text = image.to_s
-#
-#    @text.split("\n").each do |line|
-#      # save into parsed line rows
-#      # potentially offload into another job(s)
-#    end
+    image = Image.find(image_id)
+
+    tmp_path = "#{Rails.root}/tmp/storage/name_of_file.jpg"
+
+    open(tmp_path, 'wb') do |file|
+      file << open(image.file.service_url).read
+    end
+
+    tesseract_image = RTesseract.new(tmp_path)
+
+    text = tesseract_image.to_s
+
+    text.split("\n").each do |line|
+      # save into parsed line rows
+      # potentially offload into another job(s)
+    end
   end
 end
