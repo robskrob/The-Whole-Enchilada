fd132e3a49ae46247dfd92bd735b03f72e8e1022
Rob Jewell <jewell.robertp@gmail.com>
2020-04-12 15:52:57 -0400

    WIP: image text parser worker

--- app/workers/image_text_parser_worker.rb
+++ app/workers/image_text_parser_worker.rb
@@ -1,8 +1,16 @@
 class ImageTextParserWorker
   include Sidekiq::Worker
 
-  def perform(name, count)
-    puts "name #{name}"
-    puts "count #{count}"
+  def perform(image_id)
+#    image = Image.find(image_id)
+#
+#    image = RTesseract.new(image.path)
+#
+#    @text = image.to_s
+#
+#    @text.split("\n").each do |line|
+#      # save into parsed line rows
+#      # potentially offload into another job(s)
+#    end
   end
 end
