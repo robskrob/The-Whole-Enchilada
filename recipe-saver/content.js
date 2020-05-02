fca682e8a6e2939613c72b9aa0c96c6be9f2f53c
Rob Jewell <jewell.robertp@gmail.com>
2020-05-02 19:53:32 -0400

    WIP: save images to app from chrome extension

--- recipe-saver/content.js
+++ recipe-saver/content.js
@@ -1,14 +1,27 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  console.log('the request')
-  let body = document.querySelector('body')
+  console.log('request', request);
+  console.log('sender', sender);
+  if (request.action === 'getInnerHTML') {
+    let body = document.querySelector('body');
 
-//  console.log(body);
-//  console.log(body.innerText)
+    sendResponse({
+      content: body.innerText,
+      host_origin: window.location.host,
+      name: window.location.host + window.location.pathname,
+      pathname: window.location.pathname
+    });
+  }
 
-  sendResponse({
-    content: body.innerText,
-    host_origin: window.location.host,
-    name: window.location.host + window.location.pathname,
-    pathname: window.location.pathname
-  });
+  if (request.action === 'getImageElements') {
+    let imageDataArray = Array.from(document.querySelectorAll('img')).map(function(imageElement) {
+      return {
+        source: imageElement.getAttribute('src'),
+        alt: imageElement.getAttribute('alt')
+      }
+    })
+
+    sendResponse({
+      imageDataArray
+    });
+  }
 });
--- recipe-saver/popup.js
+++ recipe-saver/popup.js
@@ -1,41 +1,60 @@
-function hello() {
-  console.log('hello!!');
+function findOrCreateRecipe() {
 
-  var query = { active: true, currentWindow: true };
-
-  function callback(tabs) {
+  function postRecipeTextCallback(tabs) {
     var currentTab = tabs[0]; // there will be only one in this array
     console.log(currentTab); // also has properties like currentTab.id
 
-    chrome.tabs.sendRequest(currentTab.id, {action: "getDOM"}, function(response) {
+    chrome.tabs.sendRequest(currentTab.id, {action: "getInnerHTML"}, function(response) {
+      console.log('Response from active  tab', response);
+//      console.log('TEXT FROM ACTIVE TAB', response.content);
 
-      console.log('TEXT FROM ACTIVE TAB', response.content);
-
-      $.ajax({
-        method: 'POST',
-        url: "https://282a1084.ngrok.io/api/v1/web_recipes",
-        data: {
-          content: response.content,
-          host_origin: response.host_origin,
-          name: response.name,
-          pathname: response.pathname
-        }
-      }).done(function( data ) {
-        console.log('RESPONSE FROM THE WHOLE ENCHILADA', data)
-      });
+//      $.ajax({
+//        method: 'POST',
+//        url: "https://c03cc354.ngrok.io/api/v1/web_recipes",
+//        data: {
+//          content: response.content,
+//          host_origin: response.host_origin,
+//          name: response.name,
+//          pathname: response.pathname
+//        }
+//      }).done(function( data ) {
+//        console.log('RESPONSE FROM THE WHOLE ENCHILADA', data)
+//      });
     });
-
-//    $.ajax({
-//      url: "https://google.com"
-//    }).done(function( data ) {
-//      console.log(data)
-//    });
   }
 
-  chrome.tabs.query(query, callback);
+  chrome.tabs.query({active: true, currentWindow: true}, postRecipeTextCallback);
 
 }
 
+function queryImages() {
+  function queryImagesFromActiveTab(tabs) {
+    var currentTab = tabs[0]; // there will be only one in this array
+    console.log(currentTab); // also has properties like currentTab.id
+
+    chrome.tabs.sendRequest(currentTab.id, {action: "getImageElements"}, function(response) {
+      console.log('image elements', response);
+      var imagesSectionElement = document.querySelector('.WholeEnchiladaRecipes_images-section--KZzQjJcI2kdN0Qnn7X7AgyoO8W6VBwuOPIcyztpKBaUp')
+
+      response.imageDataArray.forEach(function(imageData) {
+        var imageElementString = "<img src='" +
+          imageData.source +
+          "' alt='" + imageData.alt + "' class='image--KZzQjJcI2kdN0Qnn7X7AgyoO8W6VBwuOPIcyztpKBaUp'>";
+
+        imagesSectionElement.insertAdjacentHTML('afterbegin', imageElementString);
+      })
+
+//      Array.from(document.querySelectorAll('img.image--KZzQjJcI2kdN0Qnn7X7AgyoO8W6VBwuOPIcyztpKBaUp')).forEach(function(imageElement) {
+//        imageElement.classList.add('')
+//      })
+    });
+  }
+
+  chrome.tabs.query({active: true, currentWindow: true}, queryImagesFromActiveTab);
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.querySelector('#clickme').addEventListener('click', hello);
+  document.querySelector('#clickme').addEventListener('click', findOrCreateRecipe);
+  document.querySelector('#get-images').addEventListener('click', queryImages);
 });
