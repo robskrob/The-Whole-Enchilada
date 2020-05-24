502c605b1705825e33f782f4787c7525015751e8
Rob Jewell <jewell.robertp@gmail.com>
2020-05-24 14:28:38 -0400

    show messages to existing user trying to
    create an account and an uncomfirmed user trying
    to sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,19 @@ class RegistrationsController < Devise::RegistrationsController
   def new
     render :new
   end
+
+  def respond_with(resource, *)
+    if resource.persisted?
+      if resource.active_for_authentication?
+        flash[:notice] = "You're signed in!"
+        redirect_to root_path
+      else
+        flash[:alert] = resource.errors.full_messages
+        redirect_to root_path
+      end
+    else
+      flash[:error] = resource.errors.full_messages.join(' ')
+      render :new
+    end
+  end
 end
--- app/services/custom_authentication_failure.rb
+++ app/services/custom_authentication_failure.rb
@@ -1,7 +1,13 @@
 class CustomAuthenticationFailure < Devise::FailureApp
   def respond
-    if warden_options[:action].to_s == "unauthenticated" && i18n_message == "Invalid Email or password."
+    invalid_log_in = warden_options[:action].to_s == "unauthenticated" && warden_options[:message].to_s == 'invalid'
+    unconfirmed_account = warden_options[:action].to_s == "unauthenticated" &&  warden_options[:message].to_s == 'unconfirmed'
+
+    if invalid_log_in
       flash[:alert] = i18n_message
+      redirect_to new_session_path
+    elsif unconfirmed_account
+      flash[:alert] = i18n_message + " Please check your email."
       redirect_to new_session_path
     end
   end
