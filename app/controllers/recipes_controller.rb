1e82ff40cdcfad41b4e6b97c7c54983db09c79d2
Rob Jewell <jewell.robertp@gmail.com>
2020-05-31 17:10:10 -0400

    set up account view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:index]
 
   def create
     @recipe = Recipe.new(
