a01c5786716639ee923c86ae2fb752a33cc005f4
Rob Jewell <jewell.robertp@gmail.com>
2020-06-21 18:06:01 -0400

    WIP: mobile layout recipe editor

--- app/javascript/packs/app/recipe_editor.js
+++ app/javascript/packs/app/recipe_editor.js
@@ -1,7 +1,41 @@
 import {deleteJson, patchJson, postJson} from './ajax.js'
 
+var observe;
+if (window.attachEvent) {
+  observe = function (element, event, handler) {
+    element.attachEvent('on'+event, handler);
+  };
+}
+else {
+  observe = function (element, event, handler) {
+    element.addEventListener(event, handler, false);
+  };
+}
+
+function init (text) {
+  function resize () {
+    text.style.height = 'auto';
+    text.style.height = text.scrollHeight+'px';
+  }
+
+  /* 0-timeout to get the already changed text */
+  function delayedResize () {
+    window.setTimeout(resize, 0);
+  }
+  observe(text, 'change',  resize);
+  observe(text, 'cut',     delayedResize);
+  observe(text, 'paste',   delayedResize);
+  observe(text, 'drop',    delayedResize);
+  observe(text, 'keydown', delayedResize);
+
+  text.focus();
+  text.select();
+  resize();
+}
+
 document.addEventListener("turbolinks:load", (_event) => {
   if (document.querySelector(".js-RecipeEditor")) {
+
     document.querySelector('.js-RecipeView__ingredient-create-button').addEventListener('click', (event) => {
       event.preventDefault()
 
@@ -189,6 +223,11 @@ document.addEventListener("turbolinks:load", (_event) => {
     let tabs = document.querySelectorAll('[data-tab-target]')
     let tabContents = document.querySelectorAll('[data-tab-content]')
 
+    Array.from(document.querySelectorAll('textarea')).forEach((textElement) => {
+      init(textElement);
+    });
+
+
     tabs.forEach(tab => {
       tab.addEventListener('click', () => {
         const tabContent = document.querySelector(tab.dataset.tabTarget)
@@ -203,8 +242,14 @@ document.addEventListener("turbolinks:load", (_event) => {
 
         tab.classList.add('RecipeView__active-tab-label')
         tabContent.classList.add('RecipeView__active-tab-content')
+
+        Array.from(document.querySelectorAll('textarea')).forEach((textElement) => {
+          init(textElement);
+        });
       })
     })
+
+
 
   }
 })
