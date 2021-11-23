b9be99d39e42d370ead4e343faf112b0bef24c63
Rob Jewell <jewell.robertp@gmail.com>
2021-11-23 13:02:18 -0500

    update email language

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('INTERNAL_MAILER_FROM', 'info@thewholeenchilada.com')
+  default from: ENV.fetch('INTERNAL_MAILER_FROM', 'info@thewholeenchilada.cc')
   layout 'mailer'
 end
