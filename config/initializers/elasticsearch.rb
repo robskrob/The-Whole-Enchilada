115e2b070d6a332710f574e7ab4e6b110d99a90d
robskrob <jewell.robertp@gmail.com>
2022-10-30 15:09:36 -0400

    elasticsearch client server version issue

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,12 @@
 if Rails.env == 'production'
   ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+
+end
+
+module Elasticsearch
+  class Client
+    def verify_with_version_or_header(body, version, headers)
+      @verified = true
+    end
+  end
 end
